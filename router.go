c7f101621ecf0f0137138e89a5bd2ecde71bd27b
admcurlyvld <gzdec337@mail.ru>
2025-10-18 19:43:12 +0400

    init commit

---
+++ router.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Welcome to Go + Gin 👋")
+	})
+
+	r.GET("/hello/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, " + name + "!",
+		})
+	})
+
+	return r
+}
